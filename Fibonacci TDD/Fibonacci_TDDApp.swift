b231a17a9002e754a0fa5310f95e7cee0a048b42
Resn4R <purview-joust.0x@icloud.com>
2023-11-15 12:42:12 +0000

    Initial Commit

---
+++ Fibonacci TDD/Fibonacci_TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fibonacci_TDDApp.swift
+//  Fibonacci TDD
+//
+//  Created by Vito Borghi on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Fibonacci_TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
