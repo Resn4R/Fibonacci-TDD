826e0c9a5e2b4bd4d3ae563f9b509fe9a011b376
Resn4R <purview-joust.0x@icloud.com>
2023-11-15 13:20:11 +0000

    code refactor

---
+++ Fibonacci TDD/FibonacciSequence.swift
@@ -0,0 +1,22 @@
+//
+//  FibonacciSequence.swift
+//  Fibonacci TDD
+//
+//  Created by Vito Borghi on 15/11/2023.
+//
+
+import Foundation
+
+struct FibonacciSequence {
+    func getFibonacciNumber(atIndex position: Int) -> Int {
+        
+        switch position {
+            
+        case 0: 0
+        case 1: 1
+        case 2: 1
+            
+        default: getFibonacciNumber(atIndex: position - 1) + getFibonacciNumber(atIndex: position - 2)
+        }
+    }
+}
